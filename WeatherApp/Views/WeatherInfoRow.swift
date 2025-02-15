d6c65ad0738087a87d741403a0d873e690ef31ac
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2025-02-15 18:15:49 +0300

    feat(ui): Add weather info row

--- WeatherApp/Views/WeatherInfoRow.swift
+++ WeatherApp/Views/WeatherInfoRow.swift
@@ -5,4 +5,27 @@
 //  Created by Sevde Aydın on 2/15/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherInfoRow: View {
+    let icon: String
+    let title: String
+    let value: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(.white)
+                .frame(width: 30)
+            
+            Text(title)
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Text(value)
+                .foregroundColor(.white)
+                .bold()
+        }
+    }
+}
